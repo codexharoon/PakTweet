eaf26d6703e9a1d2c92bd4cc9d8fbe5d2079bfcd
Muhammad Haroon <theharoonsocial@gmail.com>
2024-08-06 18:01:56 +0500

    models + lucia-auth setup + zod schemas

---
+++ src/auth.ts
@@ -0,0 +1,76 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import prisma from "./lib/prisma";
+import { Lucia, Session, User } from "lucia";
+import { cache } from "react";
+import { cookies } from "next/headers";
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false, // session cookies have very long lifespan (2 years)
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      id: attributes.id,
+      username: attributes.username,
+      displayName: attributes.displayName,
+      googleId: attributes.googleId,
+      avatarUrl: attributes.avatarUrl,
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: String;
+  username: String;
+  displayName: String;
+  googleId: String | null;
+  avatarUrl: String | null;
+}
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    // next.js throws when you attempt to set cookie when rendering page
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch {}
+    return result;
+  },
+);
---
+++ src/schema/zodValidation.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+// signup schema
+
+export const signUpSchema = z.object({
+  username: z
+    .string()
+    .min(3)
+    .regex(/^[a-zA-Z0-9_-]*$/, {
+      message: "Username can only contain letters, numbers, _ or -",
+    }),
+  email: z.string().trim().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+export type signUpSchemaType = z.infer<typeof signUpSchema>;
+
+// signin schema
+
+export const signInSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+export type signInSchemaType = z.infer<typeof signInSchema>;
