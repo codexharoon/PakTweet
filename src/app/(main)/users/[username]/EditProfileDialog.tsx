502028f793f5de4b7bb892a6d0890a6f1550883f
Muhammad Haroon <theharoonsocial@gmail.com>
2024-08-22 16:30:23 +0500

    avatar input

--- src/app/(main)/users/[username]/EditProfileDialog.tsx
+++ src/app/(main)/users/[username]/EditProfileDialog.tsx
@@ -24,6 +24,11 @@ import {
 } from "@/schema/zodValidation";
 import LoadingButton from "@/components/ui/loading-button";
 import { Textarea } from "@/components/ui/textarea";
+import Image, { StaticImageData } from "next/image";
+import { useRef, useState } from "react";
+import { Label } from "@/components/ui/label";
+import AvatarPlaceholder from "@/assets/avatar-placeholder.png";
+import { Camera } from "lucide-react";
 
 interface EditProfileDialogProps {
   user: UserProp;
@@ -33,6 +38,8 @@ interface EditProfileDialogProps {
 
 const EditProfileDialog = ({ user, open, onClose }: EditProfileDialogProps) => {
   const mutation = useUpdateUserProfileMutation();
+
+  const [croppedAvatar, setCroppedAvatar] = useState<Blob | null>(null);
 
   const form = useForm<updateUserProfileSchemaType>({
     resolver: zodResolver(updateUserProfileSchema),
@@ -66,6 +73,18 @@ const EditProfileDialog = ({ user, open, onClose }: EditProfileDialogProps) => {
         <DialogHeader>
           <DialogTitle>Update Profile</DialogTitle>
         </DialogHeader>
+
+        <div className="space-y-1.5">
+          <Label>Avatar</Label>
+          <AvatarInput
+            src={
+              croppedAvatar
+                ? URL.createObjectURL(croppedAvatar)
+                : user.avatarUrl || AvatarPlaceholder
+            }
+            onImageCropped={setCroppedAvatar}
+          />
+        </div>
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
@@ -113,3 +132,52 @@ const EditProfileDialog = ({ user, open, onClose }: EditProfileDialogProps) => {
 };
 
 export default EditProfileDialog;
+
+// avatar input
+
+interface AvatarInputProps {
+  src: string | StaticImageData;
+  onImageCropped: (blob: Blob | null) => void;
+}
+
+function AvatarInput({ src, onImageCropped }: AvatarInputProps) {
+  const [imageToCrop, setImageToCrop] = useState<File>();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function onImageSelected(image: File | undefined) {
+    if (!image) return;
+
+    //
+  }
+
+  return (
+    <>
+      <input
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={(e) => onImageSelected(e.target.files?.[0])}
+        className="sr-only hidden"
+      />
+
+      <button
+        type="button"
+        onClick={() => inputRef.current?.click()}
+        className="group relative block"
+      >
+        <Image
+          src={src}
+          alt="avatar preview/change"
+          width={150}
+          height={150}
+          className="size-32 flex-none rounded-full object-cover"
+        />
+
+        <span className="absolute inset-0 m-auto flex size-12 items-center justify-center rounded-full bg-black/50 transition-colors duration-200 group-hover:bg-black/30">
+          <Camera size={24} />
+        </span>
+      </button>
+    </>
+  );
+}
