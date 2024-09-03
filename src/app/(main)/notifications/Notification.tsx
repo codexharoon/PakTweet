0cc1c4bbbabbe026873e7f29938dafd4683f5fe5
Muhammad Haroon <theharoonsocial@gmail.com>
2024-09-03 19:36:38 +0500

    completed notification page

---
+++ src/app/(main)/notifications/Notification.tsx
@@ -0,0 +1,70 @@
+import UserAvatar from "@/components/UserAvatar";
+import { NotificationProp } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import { NotificationType } from "@prisma/client";
+import { Bookmark, Heart, MessageCircle, User2 } from "lucide-react";
+import Link from "next/link";
+
+interface NotificationProps {
+  notification: NotificationProp;
+}
+
+const Notification = ({ notification }: NotificationProps) => {
+  const NotificationTypeMap: Record<
+    NotificationType,
+    { message: string; icon: React.JSX.Element; href: string }
+  > = {
+    FOLLOW: {
+      message: `${notification.issuer.displayName} started following you.`,
+      icon: <User2 className="size-7 text-primary" />,
+      href: `/users/${notification.issuer.username}`,
+    },
+    LIKE: {
+      message: `${notification.issuer.displayName} liked your post.`,
+      icon: <Heart className="size-7 fill-red-500 text-red-500" />,
+      href: `/posts/${notification.postId}`,
+    },
+    COMMENT: {
+      message: `${notification.issuer.displayName} commented on your post.`,
+      icon: <MessageCircle className="size-7 fill-primary text-primary" />,
+      href: `/posts/${notification.postId}`,
+    },
+    BOOKMARK: {
+      message: `${notification.issuer.displayName} bookmarked your post.`,
+      icon: <Bookmark className="size-7 fill-primary text-primary" />,
+      href: `/posts/${notification.postId}`,
+    },
+  };
+
+  const { message, icon, href } = NotificationTypeMap[notification.type];
+
+  return (
+    <Link href={href} className="block">
+      <article
+        className={cn(
+          "flex gap-3 rounded-2xl bg-card p-3 shadow-sm transition-colors hover:bg-card/70",
+          !notification.read && "bg-primary/10",
+        )}
+      >
+        <div className="my-1">{icon}</div>
+
+        <div className="space-y-3">
+          <UserAvatar avatarUrl={notification.issuer.avatarUrl} size={36} />
+
+          <div>
+            <span className="font-bold">{notification.issuer.displayName}</span>{" "}
+            <span>{message}</span>
+          </div>
+
+          {notification.post && (
+            <div className="line-clamp-3 whitespace-pre-line text-muted-foreground">
+              {notification.post.content}
+            </div>
+          )}
+        </div>
+      </article>
+    </Link>
+  );
+};
+
+export default Notification;
---
+++ src/app/(main)/notifications/Notifications.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { NotificationDataProp, PostDataProp } from "@/lib/types";
+import Post from "@/components/posts/Post";
+import { kyInstance } from "@/lib/ky";
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import PostsLoadingSkeleton, {
+  PostLoadingSkeleton,
+} from "@/components/posts/PostsLoadingSkeleton";
+import Notification from "./Notification";
+
+const Notifications = () => {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["notifications"],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get("/api/notifications", {
+          searchParams: pageParam ? { cursor: pageParam } : {},
+        })
+        .json<NotificationDataProp>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+
+  if (status === "pending") {
+    return <PostsLoadingSkeleton />;
+  }
+
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">
+        An error occurred while fetching the notifications. Please try again
+        later.
+      </p>
+    );
+  }
+
+  const notifications = data.pages.flatMap((page) => page.notifications);
+
+  if (status === "success" && !hasNextPage && notifications.length === 0) {
+    return (
+      <p className="text-center text-muted-foreground">No notifications yet.</p>
+    );
+  }
+
+  return (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {notifications.map((notification) => (
+        <Notification key={notification.id} notification={notification} />
+      ))}
+
+      {isFetchingNextPage && <PostLoadingSkeleton />}
+    </InfiniteScrollContainer>
+  );
+};
+
+export default Notifications;
